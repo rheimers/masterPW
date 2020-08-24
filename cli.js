cf22d6a6fe6636fd3450cd8094ce1e48fab6e0cf
rheimers <regineheimers@gmail.com>
2020-08-24 14:08:04 +0200

    Ignore passwords.json

--- cli.js
+++ cli.js
@@ -6,7 +6,17 @@ const questions = [
     name: "password",
     message: "What's your password",
   },
+  {
+    type: "input",
+    name: "key",
+    message: "Which password?",
+  },
 ];
 inquirer.prompt(questions).then((answers) => {
-  console.log("Your password is ${answers.password}");
+  console.log(`Your password is ${answers.password}`);
+  if (answers.password === "123") {
+  } else {
+    console.log("Something went wrong");
+  }
+  console.log(`You like to know the password of ${answers.key}!`);
 });
