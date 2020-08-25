f18a82a78db61d47cae30bd9c630a2136217b2f3
rheimers <regineheimers@gmail.com>
2020-08-25 12:21:51 +0200

    Read passwords and refactoring

--- cli.js
+++ cli.js
@@ -1,28 +1,67 @@
 const inquirer = require("inquirer");
 const fs = require("fs").promises;
 
-const questions = [
+const OPTION_READ = "Read password";
+const OPTION_SET = "Set new password";
+
+const questionsIntro = [
   {
-    type: "input",
+    type: "password",
     name: "password",
-    message: "What's your password",
+    message: "What's your master password?",
   },
+
+  {
+    type: "list",
+    name: "action",
+    message: "What's your next step?",
+    choices: [OPTION_READ, OPTION_SET],
+  },
+];
+
+const questionsRetrieve = [
   {
     type: "input",
     name: "key",
-    message: "Which password?",
+    message: "Which passwprd do you need?",
   },
 ];
-inquirer.prompt(questions).then(async (answers) => {
-  if (answers.password === "123") {
+
+const questionsSet = [
+  {
+    type: "input",
+    name: "key",
+    message: "Which password do you like to set?",
+  },
+  {
+    type: "password",
+    name: "password",
+    message: "Please enter the password ",
+  },
+];
+
+inquirer.prompt(questionsIntro).then(async (answersStart) => {
+  if (answersStart.password === "123") {
     console.log("Master Password is correct!");
-    try {
-      const passwordsJSON = await fs.readFile("./passwords.json", "utf-8");
-      const passwords = JSON.parse(passwordsJSON);
-      console.log(`Your ${answers.key} password is ${passwords[answers.key]}`);
-    } catch (error) {
-      console.error("Sorry, something went wrong");
-      // What to do now?
+    if (answersStart.action === OPTION_READ) {
+      console.log("Now Get a password");
+      inquirer.prompt(questionsRetrieve).then(async (answersGet) => {
+        try {
+          const passwordsJSON = await fs.readFile("./passwords.json", "utf-8");
+          const passwords = JSON.parse(passwordsJSON);
+          console.log(
+            `Your ${answersGet.key} password is ${passwords[answersGet.key]}`
+          );
+        } catch (error) {
+          console.error("Something went wrong 😑");
+          // What to do now?
+        }
+      });
+    } else if (answersStart.action === OPTION_SET) {
+      console.log("Now Set a password");
+      inquirer.prompt(questionsSet).then(async (answersSet) => {
+        console.log(`New Password: ${answersSet.key} = ${answersSet.password}`);
+      });
     }
   } else {
     console.log("Master Password is incorrect!");
