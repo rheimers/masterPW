ed2c3f9c658ccce025eae203dde0a82233ee64cb
rheimers <regineheimers@gmail.com>
2020-08-27 14:59:18 +0200

    Install nodemon, add nodemon script

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const { repsonse, request, response } = require("express");
+
 const app = express();
 
 const port = 3000;
+
+app.get("/", (request, response) => {
+  console.log("Request /");
+  response.send("Request succeeded");
+});
 
 app.listen(3000, function () {
   console.log(`Ready! App is listening on http://localhost:${port}`);
