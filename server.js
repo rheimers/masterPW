71887b8547f2dd47d27fda225042b77fa6cd23c9
rheimers <regineheimers@gmail.com>
2020-08-31 09:26:52 +0200

    Fix depreciation warning

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-const express = require("express");
-const { repsonse, request, response } = require("express");
+require("dotenv").config();
 
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const { writePassword } = require("./lib/passwords");
+const { encrypt } = require("./lib/crypto");
+const bodyParser = require("body-parser");
+
+const client = new MongoClient(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
+});
 const app = express();
+app.use(bodyParser.json());
 
 const port = 3000;
 
-app.get("/", (request, response) => {
-  console.log("Request /");
-  response.send("Request succeeded");
-});
+async function main() {
+  await client.connect();
+  const database = client.db(process.env.MONGO_DB_NAME);
+  const masterPassword = process.env.MASTER_PASSWORD;
 
-app.listen(3000, function () {
-  console.log(`Ready! App is listening on http://localhost:${port}`);
-});
+  app.post("/api/passwords", async (request, response) => {
+    console.log("POST on /api/passwords");
+    const { name, value } = request.body;
+    const encryptedPassword = encrypt(value, masterPassword);
+    await writePassword(name, encryptedPassword, database);
+    response.status(201).send("Password created");
+  });
+
+  app.listen(port, function () {
+    console.log(`Ready! App is listening on http://localhost:${port}`);
+  });
+}
+main();
